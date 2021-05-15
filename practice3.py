75547c438cb1e7f2a92dc3f13d585b8275d10b31
Mónica Sánchez Santos <monni.sa.89@gmail.com>
2021-05-14 20:41:02 -0500

    fix practice3.py

--- practice3.py
+++ practice3.py
@@ -1,6 +1,6 @@
 """ 
 Muestra la distancia a la que dos objetos circulares (bala y enemigo) 
-deberian de estar para NO chocar y la distancia a la que realmente 
+deberian de estar para no chocar y la distancia a la que realmente 
 están. 
 
 Puedes pedir las siguientes entradas:
@@ -8,6 +8,8 @@ x,y de la bala y enemigo
 radio de bala y enemigo
 """
 from math import sqrt 
+
+print ("Vamos a calcular la distancia para matar al enemigo. Favor de darme las coordenadas solicitadas.")
 
 x_enemigo = int(input ("X enemigo:"))
 y_enemigo = int(input ("Y enemigo:"))
@@ -17,7 +19,15 @@ x_bala = int(input ("X bala:"))
 y_bala = int(input ("Y bala:"))
 r_bala = int ( input ("cual es el radio de la bala:"))
 
+hipotenusa = r_bala + r_enemigo
 a_cuad = (x_bala - x_enemigo)**2
 b_cuad = (y_bala - y_enemigo)**2
 d = sqrt(a_cuad+b_cuad)
-hipocrecia = r_bala + r_enemigo
+print (f"la distancia entre el enemigo y la bala es de {d}, si su distancia critica es de {hipotenusa}, por lo tanto el enemigo...")
+
+chocar = d - hipotenusa
+
+if (chocar < 0):
+  print ("muere")
+else: 
+  print ("sobrevive")
