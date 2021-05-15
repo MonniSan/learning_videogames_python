cdc99c50fa0137c345558e254004380998ebd519
Mónica Sánchez Santos <monni.sa.89@gmail.com>
2021-05-14 21:24:33 -0500

    fix practice4.py

--- practice4.py
+++ practice4.py
@@ -5,20 +5,33 @@ Si lo están entraran a una batalla. para poder saber si esta
 cerca el mundo cuenta con una reticula. Movimientos solo 
 pueden ser adelante, atras, arriba, abajo. 
 
-El programa pregunta X y Y del playeer y se le indica la 
-distancia Manhattan a la que se encuentran. 
+El programa pregunta X y Y del player y se le indica la 
+distancia Manhattan a la que se encuentran.
+
+__|__|__|__|__X
+__|__|__|__|__|
+__|__|__|__|__|
+__|__|__|__|__|
+Y_|__|__|__|__|
 
 """
 from math import sqrt 
 
-x_player = int(input ("X player:"))
-y_player = int(input ("Y player:"))
+print("Debemos calcular la distancia del enemigo para entrar en combate. Introduce tus coordenas y las del enemigo")
 
-x_enemigo = int(input ("X enemigo:"))
-y_enemigo = int(input ("Y enemigo:"))
+x_player = int(input ("mi coordenada X:"))
+y_player = int(input ("mi coordenada Y:"))
+
+x_enemigo = int(input ("coordenada X del enemigo:"))
+y_enemigo = int(input ("coordenada Y del enemigo:"))
 
 a = (x_player - x_enemigo)
 b = (y_player - y_enemigo)
 d = abs(a) + abs(b)
-print (f"el enemigo esta a {d} casillas")
 
+print (f"el enemigo esta a {d} casillas, por lo tanto...")
+
+if (d==0):
+  print ("entran en combate")
+else:
+  print ("no entran en combate")
