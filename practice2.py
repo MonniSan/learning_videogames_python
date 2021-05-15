bf47fa7980e50c1b79705c4a54db8800ed5b66ce
Mónica Sánchez Santos <monni.sa.89@gmail.com>
2021-05-14 20:04:12 -0500

    fix practice2.py

--- practice2.py
+++ practice2.py
@@ -1,7 +1,7 @@
 """
 Hay un hechicero que tiene un caldero. El caldero se llama RGB,
-El usuario debe indicar al hechicero la cantidad de cada uno de los componentes del hechizo que colocara
-en el caldero RGB
+El usuario debe indicar al hechicero la cantidad de cada uno de los 
+componentes del hechizo que colocara en el caldero RGB
 
 0-> 255 Ml {Rooibos, Greenish, Boost} > 765 ml
 
@@ -11,11 +11,11 @@ Cual es el color final lineal de acuerdo a cantidades posterior al hechizo:
 
 Formula = ColorFinal  = (QtyR)/? + QtyG*255/?  + QtyB*255*255/?
 """
+print ("Bienvenido, ayuda al Hechicero con su pocion.")
+rooibos = int ( input ("Indica la cantidad de Rooibos [0-255]:"))
+greenish = int ( input ("Indica la cantidad de Greenish[0-255]:"))
+boost = int ( input ("Indica la cantidad de Boost[0-255]:"))
 
-rooibos = int ( input ("indica la cantidad de rooibos [0-255]:"))
-greenish = int ( input ("indica la cantidad de greenish[0-255]:"))
-boost = int ( input ("indica la cantidad de boost[0-255]:"))
-
-ML_Totales = rooibos + greenish + boost
-ColorFinal = rooibos/ML_Totales + greenish/ML_Totales*255 + boost/ML_Totales*255*255
-print (f"La pocion tiene {ML_Totales} ml y es de color {ColorFinal}")
+Ml_Totales = rooibos + greenish + boost
+ColorFinal = rooibos/Ml_Totales + greenish/Ml_Totales*255 + boost/Ml_Totales*255*255
+print (f"La pocion tiene {Ml_Totales} ml y es de color es {ColorFinal}")
