95b61a1a85cdf48878681e90c5731467eb678ab3
Mónica Sánchez Santos <monni.sa.89@gmail.com>
2021-06-26 03:55:57 -0500

    base/setup/update

--- clase20210619/PandaTest1.py
+++ clase20210619/PandaTest1.py
@@ -3,11 +3,15 @@ from direct.task import Task
 from panda3d.core import Point3
 from panda3d.core import loadPrcFileData
 
-loadPrcFileData(" " , "win-size 800 600")
+loadPrcFileData(" " , "win-size 640 480")
 
 class FirstApp(ShowBase):
+  
   def __init__(self):
     super().__init__(self)
+    base.messenger.toggleVerbose()
+    self.taskMgr.add(self.setup, "setup")
+    self.taskMgr.add(self.update,"update")
 
 t = FirstApp()
 t.run()
