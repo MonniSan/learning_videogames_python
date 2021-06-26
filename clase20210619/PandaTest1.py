eea93e5874ca5747017413e4c31b4791ce0be402
Mónica Sánchez Santos <monni.sa.89@gmail.com>
2021-06-26 03:49:35 -0500

    change size

--- clase20210619/PandaTest1.py
+++ clase20210619/PandaTest1.py
@@ -3,11 +3,11 @@ from direct.task import Task
 from panda3d.core import Point3
 from panda3d.core import loadPrcFileData
 
+loadPrcFileData(" " , "win-size 800 600")
+
 class FirstApp(ShowBase):
   def __init__(self):
     super().__init__(self)
 
 t = FirstApp()
 t.run()
-
-
--- clase20210619/PandaText1.py
+++
@@ -1,8 +0,0 @@
-from direct.showbase.ShowBase import ShowBase
-from direct.task import Task
-from panda3d.core import Point3
-from panda3d.core import loadPrcFileData
-
-class FirstApp(ShowBase):
-  def __init__(self):
-    super().__init__(self)
